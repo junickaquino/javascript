26eeddd355422c38c73f3dbd47f8249b2bd1ca9d
junickaquino <junickaquino29@gmail.com>
2022-03-04 08:11:39 +0800

    Traversy Media - DOM Single Element Selectors

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 // ==================
-// Object Oriented Programming in ES6+
-// with classes
+// DOM - Document Object Model
 // ==================
+
+/*
+console.log(window);
+
+window.alert(1);
+ or simply alert(1) because window is the very top object
+*/
+
+// Single Element selectors
+const form = document.getElementById("my-form");
+const container = document.querySelector(".container"); // Selects the first #id, .class, element
+
+console.log(form);
+console.log(container);
+
+// Multiple Element selectors
