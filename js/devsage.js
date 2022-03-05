b259a661517ab191152dbfbf566a834f5facb35b
junickaquino <junickaquino29@gmail.com>
2022-03-05 19:25:30 +0800

    Davegray - Count To Ten, Fibonacci Sequence

---
+++ js/devsage.js
@@ -0,0 +1,49 @@
+/*
+
+== The Concept ==
+
+function processDoll(doll){
+
+  // 1) Base case
+  if (found the piece of chocolate) {
+    return "Yum Yum";
+  }
+
+  // 2) Recursive call to itself
+  else if (there are no more dolls) {
+    return "No chocolate here :("
+  }
+
+  else {
+    processDoll(the smaller doll)
+  }
+}
+
+*/
+
+// ============
+// Factorial using Recursion
+// ============
+/*
+  4!
+  = 4 * 3 * 2 * 1
+  = 24
+*/
+
+function factorial(n) {
+  // Base Case
+  if (n == 1 || n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(4)); // Output: 24
+/*
+    factorial(4)
+      4 * factorial(3)
+        3 * factorial(2)
+          2 * factorial(1)
+            2 * 1
+  */
