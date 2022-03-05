bbbcb3579511775719bd916f1e24ed8c9bce2ac3
junickaquino <junickaquino29@gmail.com>
2022-03-05 22:30:31 +0800

    Dave Gray - Nth pos in Fib. IRL Ex: Token, Parser.

--- main.js
+++ main.js
@@ -65,3 +65,121 @@ const fib = (num, array = [0, 1]) => {
 };
 
 console.log(fib(12));
+
+// ==================
+// 3. Find the nth position in a Fibonacci Sequence
+//  -- Returns the number in a Fib seq for the inputted position.
+// ==================
+
+// Without Recursion
+/*
+const fibPos = (pos) => {
+  if (pos <= 1) {
+    return pos;
+  } else {
+    const seq = [0, 1];
+
+    for (let i = 2; i <= pos; i++) {
+      const [nextToLast, last] = seq.slice(-2);
+      seq.push(nextToLast + last);
+    }
+
+    return seq[pos];
+  }
+};
+
+console.log(fibPos(8));
+*/
+
+// With Recursion
+const fibPos = (pos) => {
+  if (pos < 2) {
+    return pos;
+  } else {
+    return fibPos(pos - 1) + fibPos(pos - 2);
+  }
+};
+
+console.log(fibPos(8));
+
+// One line Recursion
+const fibPos1L = (pos) => (pos < 2 ? pos : fibPos(pos - 1) + fibPos(pos - 2));
+console.log("One line: " + fibPos1L(8));
+
+// ==================
+// 4. IRL Example: Continuation Token from an API
+//  --
+// ==================
+
+// Pseudo code
+const getAWSProductIdImages = async () => {
+  /* 
+
+    <Get the data with await Fetch request>
+
+  */
+
+  if (data.IsTruncated) {
+    // Recursive Call
+    return await getAWSProductIdImages(
+      productsId,
+      s3, // connection to s3
+      resultArray, // accumulator
+      data.NextContinuationToken
+    );
+  }
+
+  return resultArray;
+};
+
+// ==================
+// 5. IRL Example: A Parser
+//  -- a company directory
+//  -- a file directory
+//  -- the DOM - a web crawler
+//  -- An XML or JSON data export
+// ==================
+
+/*
+  A function that retrieves all of artists names.
+  A recursion handles this well because there are categories and subcategories that you might not know existed.
+*/
+
+// Export from your streaming service like Spotify , YT Music, etc.
+const artistsByGenre = {
+  jazz: ["Miles Davis", "John Coltrane"],
+  rock: {
+    classic: ["Bob Seger", "The Eagles"],
+    hair: ["Def Leppard", "Whitesnake", "Poison"],
+    alt: {
+      classic: ["Pearl Jam", "The Killers"],
+      current: ["Joywave", "Sir Sly"],
+      test: {
+        // Added for testing.
+        testinner: ["Junick"], // Should now outputs 17 artists.
+      },
+    },
+  },
+
+  unclassified: {
+    new: ["Caamp", "Neil Young"],
+    classic: ["Seal", "Morcheeba", "Chris Stapleton"],
+  },
+};
+
+const getArtistNames = (dataObj, arr = []) => {
+  Object.keys(dataObj).forEach((key) => {
+    if (Array.isArray(dataObj[key])) {
+      // console.log(arr); --- uncomment for testing. Outputs array for every object reads.
+      return dataObj[key].forEach((artist) => {
+        arr.push(artist);
+      });
+    }
+
+    getArtistNames(dataObj[key], arr);
+  });
+
+  return arr;
+};
+
+console.log(getArtistNames(artistsByGenre)); // Output: all of the 16 artists in one list/array.
