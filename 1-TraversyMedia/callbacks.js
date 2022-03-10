8ebc0971a1b5a838bb1f78b3eefc4e8c7093a583
junickaquino <junickaquino29@gmail.com>
2022-03-10 23:43:47 +0800

    TraversyMedia - Async - Problem

---
+++ 1-TraversyMedia/callbacks.js
@@ -0,0 +1,39 @@
+// ===========================
+// Mimic Blogpost in a server
+// ===========================
+
+const posts = [
+  {
+    title: "Post One",
+    body: "This is post one",
+  },
+  {
+    title: "Post Two",
+    body: "This is post two",
+  },
+];
+
+function getPosts() {
+  setTimeout(() => {
+    let output = "";
+
+    posts.forEach((post, index) => {
+      output += `<li>${post.title}</li>`;
+    });
+
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+function createPost(post) {
+  setTimeout(() => {
+    posts.push(post);
+  }, 2000);
+}
+
+getPosts();
+
+createPost({
+  title: "Post Three",
+  body: "This is post three",
+});
