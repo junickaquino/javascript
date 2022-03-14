551b238af5a1872e061ac5aaaa7a70959bcc85aa
junickaquino <junickaquino29@gmail.com>
2022-03-14 23:52:29 +0800

    8Kyu - isPalindrome

--- 8Kyu/isPalindrome.js
+++ 8Kyu/isPalindrome.js
@@ -10,6 +10,17 @@ function isPalindrome(x) {
 
   // Compare the word with its reverse version
   return x === reverse ? true : false;
+  /*
+  The ternary operator is redundant since we have === already
+  to evaluate true or false.
+    return x === reverse;  <-- this is enough
+
+  /* 
+  == ONE LINER ANDY LMAO ==
+
+  return x.split("").reverse().join("").toLowerCase() === x.toLowerCase() ? true : false
+
+  */
 }
 
 console.log(isPalindrome("aba"));
