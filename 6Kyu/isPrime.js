2e16601bdeb73585a6db348c01a103076ecab252
junickaquino <junickaquino29@gmail.com>
2022-04-03 19:06:57 +0800

    6Kyu - isPrime. FAILED

--- 6Kyu/isPrime.js
+++ 6Kyu/isPrime.js
@@ -1,17 +1,36 @@
+// function isPrime(num) {
+//   if (num < 0) {
+//     return false;
+//   }
+
+//   let result = (num + 1) / 6;
+
+//   console.log(result);
+
+//   if (result % 1 == 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(isPrime(2));
+
 function isPrime(num) {
-  if (num < 0) {
+  if (num <= 1) {
     return false;
   }
 
-  let result = (num + 1) / 6;
+  const range = (min, max) =>
+    [...Array(max - min + 1).keys()].map((i) => i + min);
 
-  console.log(result);
-
-  if (result % 1 == 0) {
-    return true;
-  } else {
-    return false;
+  for (i in range(3, num)) {
+    if (num % i == 0) {
+      return false;
+    }
   }
+
+  return true;
 }
 
-console.log(isPrime(2));
+console.log(isPrime(1));
